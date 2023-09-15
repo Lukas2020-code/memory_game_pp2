a99f340f418d63a515d0a9530e66bfba7347fb67
Lukas2020-code <>
2023-09-15 22:53:27 +0000

    Create array of card objects

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,23 @@ const userName = document.getElementById("userName");
 const userMoves = document.getElementById("userMoves");
 const timeValue = document.getElementById("timer");
 const userScores = document.querySelector(".user-scores");
+
+//Array of card objects
+const cardsArray = [
+    { name: "bee", image: "assets/img/bee.png" },
+    { name: "bunny", image: "assets/img/bunny.png" },
+    { name: "lamb", image: "assets/img/lamb.png" },
+    { name: "snake", image: "assets/img/snake.png" },
+    { name: "cow", image: "assets/img/cow.png" },
+    { name: "tiger", image: "assets/img/tiger.png" },
+    { name: "dog", image: "assets/img/dog.png" },
+    { name: "cat", image: "assets/img/cat.png" },
+    { name: "bee", image: "assets/img/bee.png" },
+    { name: "bunny", image: "assets/img/bunny.png" },
+    { name: "lamb", image: "assets/img/lamb.png" },
+    { name: "snake", image: "assets/img/snake.png" },
+    { name: "cow", image: "assets/img/cow.png" },
+    { name: "tiger", image: "assets/img/tiger.png" },
+    { name: "dog", image: "assets/img/dog.png" },
+    { name: "cat", image: "assets/img/cat.png" },
+];
